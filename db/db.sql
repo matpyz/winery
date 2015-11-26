3d7c6ab98ed4ebd93b1734fd26e6722380f57b38
Piotr Srebniak <piotr@srebniak.pl>
2015-11-26 09:17:53 +0000

    db

---
+++ db/db.sql
@@ -0,0 +1,154 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Czas wygenerowania: 24 Lis 2015, 19:56
+-- Wersja serwera: 5.6.27-0ubuntu0.14.04.1
+-- Wersja PHP: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Baza danych: `zespolowe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `documents`
+--
+
+CREATE TABLE IF NOT EXISTS `documents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `document` blob,
+  `creatorId` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `documents_creator` (`creatorId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `group`
+--
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `group2permission`
+--
+
+CREATE TABLE IF NOT EXISTS `group2permission` (
+  `groupId` int(10) unsigned DEFAULT NULL,
+  `permissionId` int(10) unsigned DEFAULT NULL,
+  `access` int(11) DEFAULT NULL,
+  KEY `group2permission_permission` (`permissionId`),
+  KEY `group2permission_group` (`groupId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `run_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Zrzut danych tabeli `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `name`, `run_on`) VALUES
+(1, '/20151123195417-create-docs', '2015-11-24 19:53:51'),
+(2, '/20151124180938-create-rbac', '2015-11-24 19:53:51'),
+(4, '/20151124184751-document2user', '2015-11-24 19:55:52');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `permission`
+--
+
+CREATE TABLE IF NOT EXISTS `permission` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user2group`
+--
+
+CREATE TABLE IF NOT EXISTS `user2group` (
+  `userId` int(10) unsigned DEFAULT NULL,
+  `groupId` int(10) unsigned DEFAULT NULL,
+  KEY `user2group_group` (`groupId`),
+  KEY `user2group_user` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_creator` FOREIGN KEY (`creatorId`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `group2permission`
+--
+ALTER TABLE `group2permission`
+  ADD CONSTRAINT `group2permission_group` FOREIGN KEY (`groupId`) REFERENCES `group` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `group2permission_permission` FOREIGN KEY (`permissionId`) REFERENCES `permission` (`id`) ON DELETE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `user2group`
+--
+ALTER TABLE `user2group`
+  ADD CONSTRAINT `user2group_group` FOREIGN KEY (`groupId`) REFERENCES `group` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user2group_user` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db/migrations/20151124180938-create-rbac.js
@@ -0,0 +1,133 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  db.createTable('user',{
+    columns: {
+      id: {
+        type: 'int',
+        unsigned: true,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: 'string',
+      email: 'string',
+      password: 'string'
+    },
+    ifNotExists: true
+  });
+  
+  db.createTable('group',{
+    columns: {
+      id: {
+        type: 'int',
+        unsigned: true,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: 'string'
+    },
+    ifNotExists: true
+  });
+
+  db.createTable('permission',{
+    columns: {
+      id: {
+        type: 'int',
+        unsigned: true,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: 'string'
+    },
+    ifNotExists: true
+  });
+
+  db.createTable('user2group',{
+    columns: {
+      userId: {
+        type: 'int',
+        unsigned: true,
+        foreignKey: {
+          name: 'user2group_user',
+          table: 'user',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+      groupId: {
+        type: 'int',
+        unsigned: true,
+        foreignKey: {
+          name: 'user2group_group',
+          table: 'group',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+    },
+    ifNotExists: true
+  });
+
+  db.createTable('group2permission',{
+    columns: {
+      groupId: {
+        type: 'int',
+        unsigned: true,
+        foreignKey: {
+          name: 'group2permission_group',
+          table: 'group',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+      permissionId: {
+        type: 'int',
+        unsigned: true,
+        foreignKey: {
+          name: 'group2permission_permission',
+          table: 'permission',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+      access: 'int'
+    },
+    ifNotExists: true
+  });
+  return null;
+};
+
+exports.down = function(db) {
+  db.dropTable('group2permission');
+  db.dropTable('user2group');
+  db.dropTable('permission');
+  db.dropTable('group');
+  db.dropTable('user');
+  return null;
+};

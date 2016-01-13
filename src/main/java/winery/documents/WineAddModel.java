00e6e120686835a32fd08e15161d7a1b4a8a1163
Kwaidan00 <aleksander.spyra@gmail.com>
2016-01-13 23:44:13 +0100

    Nowa zakładka w programie - możliwość dodawania wina do bazy danych

---
+++ src/main/java/winery/documents/WineAddModel.java
@@ -0,0 +1,20 @@
+package winery.documents;
+
+import dbapi.DBManager;
+
+import winery.model.Model;
+
+public class WineAddModel extends Model {
+
+	
+	WineAddModel() {
+		
+	}
+	
+	public void firstUse() {
+
+
+	}
+	
+	
+}

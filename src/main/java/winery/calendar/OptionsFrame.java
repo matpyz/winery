f16fc942e62e652f385211d91680822f9080bbad
gothic17 <b.rzepkowski94@gmail.com>
2016-01-20 20:13:55 +0200

    Zamiana przycisków wyświetlających wszystkie godziny na tylko takie, które wyswietlają wydarzenie.

--- src/main/java/winery/calendar/OptionsFrame.java
+++ src/main/java/winery/calendar/OptionsFrame.java
@@ -43,7 +43,7 @@ public class OptionsFrame extends JFrame implements ActionListener {
 			this.calendar = calendar;
 			this.setTitle("Opcje");
 			this.getContentPane().setLayout(null);
-			this.setSize(330, 320);
+			this.setSize(400, 400);
 			this.setLocation(x, y);
 			this.actionsHolder = new JButton[24];
 						
@@ -52,7 +52,7 @@ public class OptionsFrame extends JFrame implements ActionListener {
 			actions.setLayout(new BoxLayout(actions, BoxLayout.Y_AXIS));
 			
 			Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-			
+			/*
 			for (int i = 0; i < 24; i++) {
 				if(i<10) {
 					actionsHolder[i] = new JButton("0" + Integer.toString(i) + ":00");
@@ -66,37 +66,61 @@ public class OptionsFrame extends JFrame implements ActionListener {
 				actionsHolder[i].addActionListener(this);
 				actionsHolder[i].setActionCommand(Integer.toString(i));
 				actions.add(actionsHolder[i]);
-				
+				*/
+				calendar.set(Calendar.DAY_OF_MONTH, numberOfTheDay);
 				//Sprawdzenie, czy jest jakieś wydarzenie w tej godzinie
-				calendar.set(Calendar.HOUR_OF_DAY, i);
+				calendar.set(Calendar.HOUR_OF_DAY, calendar.getActualMinimum(Calendar.HOUR_OF_DAY));
 				calendar.set(Calendar.MINUTE, calendar.getActualMinimum(Calendar.MINUTE));
 				calendar.set(Calendar.SECOND, calendar.getActualMinimum(Calendar.SECOND));
 				Date startDate = new java.sql.Date(calendar.getTime().getTime());
 				System.out.println(calendar.getTime());
 				
-				calendar.set(Calendar.HOUR_OF_DAY, i+1);
+				calendar.set(Calendar.HOUR_OF_DAY, calendar.getActualMaximum(Calendar.HOUR_OF_DAY));
 				calendar.set(Calendar.MINUTE, calendar.getActualMinimum(Calendar.MINUTE));
 				calendar.set(Calendar.SECOND, calendar.getActualMinimum(Calendar.SECOND));
 				Date endDate = new java.sql.Date(calendar.getTime().getTime());
 				
-				ArrayList<Event> events = util.getAllDayEvents(startDate, endDate);
+				// Przykladowe zastepcze wydarzenia
+				ArrayList<Event> events = new ArrayList();
+				calendar.set(Calendar.HOUR_OF_DAY, 12);
+				Date startDate1 = new java.sql.Date(calendar.getTime().getTime());
+				calendar.set(Calendar.HOUR_OF_DAY, 14);
+				Date endDate1 = new java.sql.Date(calendar.getTime().getTime());
+				Event event1 = new Event(1, "Spotkanie", "Opis", startDate, endDate, "Winiarnia", 2,
+						3, "Spot-Win");
+				events.add(event1);
+				Event event2 = new Event(1, "Zbiory", "Opis", startDate, endDate, "Winiarnia", 2,
+						3, "Spot-Win");
+				events.add(event2);
+				
+				//ArrayList<Event> events = util.getAllDayEvents(startDate, endDate);
+				int i = 0;
 				for (Event event: events) {
-					actionsHolder[i].setBackground(Color.BLUE);
-					actionsHolder[i].setText(event.getName());
+					//actionsHolder[i].setBackground(Color.BLUE);
+					//actionsHolder[i].setText(event.getName());
+					actionsHolder[i] = new JButton(event.getStartDate().toString() + " - " + 
+							event.getName());
+					actionsHolder[i].setMinimumSize(new Dimension(350, 50));
+					actionsHolder[i].setMaximumSize(new Dimension(350, 50));
+					actionsHolder[i].setBorder(border);
+					actionsHolder[i].setVisible(true);
+					actionsHolder[i].addActionListener(this);
+					actionsHolder[i].setActionCommand(Integer.toString(i));
+					actions.add(actionsHolder[i]);
 				}
-			}
+			//}
 			
 			scrollPane = new JScrollPane(actions);
 			scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 	        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-			scrollPane.setBounds(15, 15, 160, 255);
+			scrollPane.setBounds(15, 15, 350, 300);
 			//scrollPane.setWheelScrollingEnabled(true);
 			getContentPane().add(scrollPane);
 			
 			//add(actions);
 			
 			add = new JButton("Dodaj");
-			add.setBounds(184, 11, 120, 35);
+			add.setBounds(260, 340, 120, 35);
 			add.setVisible(true);
 			add.addActionListener(addListener);
 			/*
